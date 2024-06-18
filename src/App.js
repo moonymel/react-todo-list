b189693773223566b040755e56b558e323c78a89
moonymel <musumeci.me@gmail.com>
2024-06-18 17:12:25 +0200

    [CREATED] project, updated app.js to test the app

---
+++ src/App.js
@@ -0,0 +1,25 @@
+function boldify(str) {
+  return <strong>{str}</strong>;
+}
+
+// COMPONENTI 
+
+function AppTitle() {
+  return <h1>Todo app</h1>;
+}
+
+function AppMessage(props) {
+  return <p>
+    <span className="hello">Benvenuta {boldify(props.name)}!</span>{" "}
+    <span className="miss">Il tuo id è il numero {props.id}.</span>
+  </p>;
+}
+
+export default function App() {
+  return (
+    <div>
+      <AppTitle />
+      <AppMessage name="Melissa" id="1" />
+    </div>
+  );
+}
